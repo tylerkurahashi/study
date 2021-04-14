53dcffba3af707cc50c0e7dad36b47d6530143f4
tylerkurahashi <tyler.kurahashi@gmail.com>
2021-04-15 00:00:16 +0900

    flask with mongoDB

--- python/flask/app.py
+++ python/flask/app.py
@@ -3,7 +3,16 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 client = MongoClient()
+print(client)
+
+finace_db = client.finance
+print(finace_db)
+sum_collection = finace_db.sum
+print(sum_collection)
+
+history = sum_collection.find_one()
+print(history)
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    return str(history['Income'])
