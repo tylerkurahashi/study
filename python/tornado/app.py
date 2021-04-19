f4325f3f7b560ffc15f8f58aa3313cbc875ed038
tylerkurahashi <tyler.kurahashi@gmail.com>
2021-04-19 21:54:12 +0900

    wip

---
+++ python/tornado/app.py
@@ -0,0 +1,21 @@
+import tornado.ioloop
+import tornado.web
+from tornado.web import RequestHandler
+
+from tornado.options import define, options, parse_command_line
+
+
+
+class MainHandler(RequestHandler):
+    def get(self):
+        self.write({'message':'Hello World'})
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
