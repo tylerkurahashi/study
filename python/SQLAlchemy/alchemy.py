74dd0c4a2c99848ffd845961c93774fc4f0386ff
tylerkurahashi <tyler.kurahashi@gmail.com>
2021-04-22 23:32:23 +0900

    alchemy wip

--- python/SQLAlchemy/alchemy.py
+++ python/SQLAlchemy/alchemy.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy import text
 
-engine = create_engine("mysql+pymysql://root:mysql@mysql:3306")
+engine = create_engine("mysql+pymysql://root:mysql@localhost:3306/TESTALCHEMY")
+# engine.connect()
 
 with engine.connect() as connection:
-    result = connection.execute(text("select username from users"))
-    for row in result:
-        print("username:", row["username"])
+    result = connection.execute(text("select user,user_id,age from myTable"))
+    for user,user_id,age in result:
+        print("user_id is:", user_id)
+        print("user is:", user)
+        print("age is:", age)
+    
