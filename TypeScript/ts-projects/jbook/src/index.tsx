b76fb79a8263ea3f63b56c796494bb6c1573ac21
tylerkurahashi <tyler.kurahashi@gmail.com>
2021-07-08 21:32:00 +0900

    wasm included into the public directory

--- TypeScript/ts-projects/jbook/src/index.tsx
+++ TypeScript/ts-projects/jbook/src/index.tsx
@@ -1,7 +1,56 @@
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <h1>Hi!</h1>;
+  const [input, setInput] = useState<string>('');
+  const [code, setCode] = useState<string>('');
+  const serviceRef = useRef<any>(null);
+  const textRef = useRef<HTMLTextAreaElement | null>(null);
+
+  useEffect(() => {
+    startService()
+
+    if (!textRef.current) {
+      return;
+    }
+
+    textRef.current.focus();
+  }, []);
+
+  const startService = async () => {
+    serviceRef.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+    
+  }
+
+  const onClick = async () => {
+    if (!serviceRef.current) {
+      return;
+    }
+    const result = await serviceRef.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015'
+    });
+
+    setCode(result.code)
+  };
+
+  return (
+    <div>
+      <textarea
+        ref={textRef}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
