61431a91865c57f224878fd74e47402b347a01f7
tylerkurahashi <tyler.kurahashi@gmail.com>
2021-09-21 12:07:59 +0900

    abc218 c.py

--- atCoder/abc218/c.py
+++ atCoder/abc218/c.py
@@ -1,19 +1,68 @@
-import sys
+def rot(S):
+  return list(zip(*S[::-1]))
 
-arr = []
-for line in sys.stdin:
-  if line == '\n':
-    break
-  arr.append(line.strip('\n'))
 
-N = arr[0]
-S = []
-T = []
+def find_left_top(S):
+  
+  for i in range(N):
+    print(f'[{i}]', S[i])
+    for j in range(N):
+      print(S[i][j])
+      if S[i][j] == '#':
+        return i, j
 
-for r in arr[1:6]:
-  S.append([e for e in r])
 
-for r in arr[6:]:
-  T.append([e for e in r])
+def is_same(S, T):
+  print('S')
+  print(S)
+  Si, Sj = find_left_top(S)
+  print('T')
+  print(T)
+  Ti, Tj = find_left_top(T)
 
-print(S, T)
+  print('Si, Sj',(Si, Sj))
+  print('Ti, Tj',(Ti, Tj))
+
+  offset_i = Ti - Si
+  offset_j = Tj - Sj
+
+  for i in range(N):
+    for j in range(N):
+      ii = i + offset_i
+      jj = j + offset_j
+
+      if 0 <= ii < N and 0 <= jj < N:
+        if S[i][j] != T[ii][jj]:
+          return False
+
+      else:
+        print('in the else condition')
+        print((i,j))
+        print(S)
+        print(T)
+        if S[i][j] == '#':
+          print('it is false')
+          return False
+
+  return True
+
+
+N = int(input())
+S = [input() for _ in range(N)]
+T = [input() for _ in range(N)]
+
+cntS = sum(1 for i in range(N) for j in range(N) if S[i][j] == '#')
+cntT = sum(1 for i in range(N) for j in range(N) if T[i][j] == '#')
+
+if cntS != cntT:
+  print("No")
+  exit()
+
+for _ in range(4):
+  if is_same(S, T):
+    print("Yes")
+    exit()
+
+  S = rot(S)
+
+print("No")
